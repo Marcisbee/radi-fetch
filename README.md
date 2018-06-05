84ed4dcdee25da9f45d14da7cca0a746b7f07e5d
Marcisbee <marcisbergmanis@gmail.com>
2018-06-06 00:01:13 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,112 @@
 # radi-fetch
+
+`radi-fetch` is the official HTTP client for [Radi.js](https://radi.js.org). It deeply integrates with Radi for seamless application building.
+
+[![npm version](https://img.shields.io/npm/v/radi-fetch.svg?style=flat-square)](https://www.npmjs.com/package/radi-fetch)
+[![npm downloads](https://img.shields.io/npm/dm/radi-fetch.svg?style=flat-square)](https://www.npmjs.com/package/radi-fetch)
+[![gzip bundle size](http://img.badgesize.io/https://unpkg.com/radi-fetch@latest/dist/radi-fetch.min.js?compression=gzip&style=flat-square)](https://unpkg.com/radi-fetch@latest/dist/radi-fetch.js)
+[![radi workspace on slack](https://img.shields.io/badge/slack-radijs-3eb891.svg?style=flat-square)](https://join.slack.com/t/radijs/shared_invite/enQtMjk3NTE2NjYxMTI2LWFmMTM5NTgwZDI5NmFlYzMzYmMxZjBhMGY0MGM2MzY5NmExY2Y0ODBjNDNmYjYxZWYxMjEyNjJhNjA5OTJjNzQ)
+
+
+## Installation
+
+To install the stable version:
+
+```
+npm install --save radi-fetch
+```
+
+This assumes you are using [npm](https://www.npmjs.com/) as your package manager.  
+
+If you're not, you can [access these files on unpkg](https://unpkg.com/radi-fetch/dist/), download them, or point your package manager to them.
+
+#### Browser Compatibility
+
+`radi-fetch` currently is compatible with browsers that support ES5.
+
+## Getting started
+
+Here is how it works. We start by initiating plugin for Radi `Radi.plugin(/* radi-fetch */, /* config */)`. Config here is optional, but we can define crucial parts of http client.
+
+```jsx
+import RadiFetch from 'radi-fetch'
+
+Radi.plugin(RadiFetch)
+```
+
+or
+
+```jsx
+import RadiFetch from 'radi-fetch'
+
+Radi.plugin(RadiFetch, {
+  /* We can define base url */
+  baseUrl: 'https://example.com',
+
+  /* We can define headers */
+  headers: {
+    'Authorization': 'key',
+  },
+
+  /* We can define dummy data that will be returned for requests made with radi-fetch */
+  dummy: true,           // Should return dummy data or use real requests
+  dummyTimeout: 1000,    // Dummy data latency simulation
+  dummyData: {           // Actual dummy data
+    get: {
+      '/api': {
+        hello: 'world',
+      },
+    },
+  },
+});
+```
+
+That's it, we're ready to go. We can start fetching our api or whatever we want by using fetch by `$fetch[type](url, params, headers)` handle.
+Response can be automatically parsed as JSON by using `.json()` method, or `.text()` for plain text response.
+
+```jsx
+class MyComponent extends Radi.Component {
+  on() {
+    return {
+      mount() {
+        this.$fetch.get('/test')
+          .then(response => {
+            console.log(response.json())
+          })
+          .catch(error => {
+            console.log('Got some', error)
+          })
+      }
+    }
+  }
+}
+```
+
+Fetch automatically sets loading state for any ongoing request. We can listen to them in radi using `$loading` handle.
+
+```jsx
+/* @jsx Radi.r */
+
+class MyComponent extends Radi.Component {
+  view() {
+    return (
+      <ul>
+        <li>/api is { this.$loading.state['/api'] ? 'loading' ? 'ready' }</li>
+        <li>Number of active requests: { this.$loading.state.$count }</li>
+        <li>Anything loading: { this.$loading.state.$any }</li>
+      </ul>
+    )
+  }
+}
+```
+
+## Stay In Touch
+
+- [Twitter](https://twitter.com/radi_js)
+- [Slack](https://join.slack.com/t/radijs/shared_invite/enQtMjk3NTE2NjYxMTI2LWFmMTM5NTgwZDI5NmFlYzMzYmMxZjBhMGY0MGM2MzY5NmExY2Y0ODBjNDNmYjYxZWYxMjEyNjJhNjA5OTJjNzQ)
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
+
+Copyright (c) 2018-present, Marcis (Marcisbee) Bergmanis
---
+++ dist/radi-fetch.js
@@ -0,0 +1,185 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global['radi-fetch'] = {})));
+}(this, (function (exports) { 'use strict';
+
+const version = '0.3.20';
+
+// Pass config to initiate things
+var index = ({
+    r,
+    l,
+    mount,
+    headless,
+    Component,
+  }, config = {}) => {
+  let prefix = (config.baseUrl || '').replace(/\/$/, '');
+  let dummy = config.dummy && (config.dummyData || {});
+
+  let fetchdummy = (type, key, cb) => {
+    setTimeout(() => {
+      cb(dummy[type] && dummy[type][key] || null);
+    }, config.dummyTimeout || 100);
+  };
+
+  let HTTP = function HTTP(t, url, params, headers, loading) {
+    this.id = url;
+    loading.start(url);
+    this.type = t;
+    this.end = () => loading.end(this.id);
+    this.http = new XMLHttpRequest();
+    this.headers = Object.assign(config.headers, headers);
+    this.params = JSON.stringify(params);
+    this.resolve = () => {
+      this.end();
+    };
+    this.reject = e => {
+      console.error('[Radi Fetch] WARN: Request caught an error.\n', e);
+      this.end();
+    };
+
+    let n = url.split('?').length - 1;
+    if (t === 'get')
+      for (let i in params) {
+        url = url.concat(((!n)?'?':'&') + i + '=' + params[i]);
+        n += 1;
+      }
+
+    this.http.open(t, prefix + url, true);
+
+    for (let h in this.headers) {
+      this.http.setRequestHeader(h, this.headers[h]);
+    }
+
+    // Allows to abort request
+    this.abort = (...args) => this.http.abort(...args);
+  };
+
+  HTTP.prototype.catch = function (ERR) {
+    if (typeof ERR === 'function') {
+      this.reject = (...args) => {
+        ERR(...args);
+        this.end();
+      };
+    }
+    return this
+  };
+
+  HTTP.prototype.then = function then(OK, ERR) {
+    this.resolve = (...args) => {
+      OK(...args);
+      this.end();
+    };
+    if (typeof ERR === 'function') {
+      this.reject = (...args) => {
+        ERR(...args);
+        this.end();
+      };
+    }
+    if (dummy) {
+      fetchdummy(this.type, this.id, data => {
+        this.resolve({
+          headers: '',
+          status: 'dummy',
+          response: JSON.stringify(data),
+          text: () => JSON.stringify(data),
+          json: () => data,
+        });
+      });
+      return this;
+    }
+    let self = this;
+    this.http.onreadystatechange = function(e) {
+      let res = this;
+      if (res.readyState === XMLHttpRequest.DONE) {
+        let h = {
+          headers: self.http.getAllResponseHeaders(),
+          status: res.status,
+          response: res.response
+        };
+        if (res.status === 200) {
+          h.text = function() { return res.responseText };
+          h.json = function() {
+            try {
+              return JSON.parse(this.text());
+            }
+            catch(error) {
+              console.error('[Radi Fetch] WARN: Response is not JSON, using fallback to empty JSON.\n', error);
+              return {};
+            }
+          };
+          self.resolve(h);
+        } else {
+          self.reject(h);
+        }
+      }
+    };
+    this.http.send(this.params);
+    return this
+  };
+
+  class Fetch extends Component {
+    get(u, p, h) { return new HTTP('get', u, p, h, this.$loading) }
+    post(u, p, h) { return new HTTP('post', u, p, h) }
+    put(u, p, h) { return new HTTP('put', u, p, h) }
+    delete(u, p, h) { return new HTTP('delete', u, p, h) }
+    options(u, p, h) { return new HTTP('options', u, p, h) }
+    head(u, p, h) { return new HTTP('head', u, p, h) }
+  }
+
+  class Loading extends Component {
+    state() {
+      return {
+        $any: false,
+        $count: 0,
+      }
+    }
+
+    start(key) {
+      if (this.state[key]) return false
+      this.setState({
+        [key]: true,
+        $count: this.state.$count + 1,
+        $any: !!(this.state.$count + 1),
+      });
+    }
+
+    end(key) {
+      this.setState({
+        [key]: false,
+        $count: this.state.$count - 1,
+        $any: !!(this.state.$count - 1),
+      });
+    }
+
+    run(key, fn) {
+      if (this.state[key]) return false
+      this.start(key);
+
+      fn(cb => {
+        if (typeof cb === 'function') cb();
+        this.end(key);
+      });
+    }
+  }
+
+  // Initiates loading component
+  headless('loading', Loading);
+
+  // Initiates fetch component
+  headless('fetch', Fetch);
+
+  return {
+    config,
+    Fetch,
+  };
+};
+
+exports.version = version;
+exports.default = index;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+//# sourceMappingURL=radi-fetch.js.map
